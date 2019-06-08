371cc40c7185b0955622668f33446238ddc66791
syo <M.youta.kwg@gmail.com>
2019-06-08 13:17:51 +0900

    不要なimportを削除

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,11 +2,10 @@ package controllers
 
 import javax.inject.Inject
 import javax.inject.Singleton
-import play.api.mvc.Action
-import play.api.mvc.AnyContent
+
 import play.api.mvc.MessagesAbstractController
 import play.api.mvc.MessagesControllerComponents
-import play.api.mvc.Request
+
 
 @Singleton
 class HomeController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
