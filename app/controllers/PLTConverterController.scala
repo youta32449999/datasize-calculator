12c120766cda16a90553af5ab04fce077de9a874
syo <M.youta.kwg@gmail.com>
2019-06-13 23:48:22 +0900

    Herokuのtmpファイルの仕様に対応するための修正

--- app/controllers/PLTConverterController.scala
+++ app/controllers/PLTConverterController.scala
@@ -21,26 +21,30 @@ class PLTConverterController @Inject()(cc: MessagesControllerComponents)(implici
     request.body.file("plt_file") match {
       case Some(uploadFile) => {
 
+        // "./tmp/plt/cache/"を作成
+        val dir = Paths.get("./tmp/plt/cache")
+        if(Files.notExists(dir)) Files.createDirectories(dir)
+
         // uploadされたfileの名前を取得
         val fileName = uploadFile.filename
 
         // uploadされたfileを一時的に保存
-        uploadFile.ref.copyTo(Paths.get(s"./tmp/cache/$fileName"), replace = true)
+        uploadFile.ref.copyTo(Paths.get(s"./tmp/plt/cache/$fileName"), replace = true)
 
         // pltをコンバート
-        val result = PLTConverter(s"./tmp/cache/$fileName")
+        val result = PLTConverter(s"./tmp/plt/cache/$fileName")
 
         // 新規ファイル作成
-        val file = Paths.get(s"./tmp/converted/$fileName")
+        val file = Paths.get(s"./tmp/plt/$fileName")
         if (Files.notExists(file)) Files.createFile(file)
 
         // 書き込み
-        val pw = new PrintWriter(s"./tmp/converted/$fileName")
+        val pw = new PrintWriter(s"./tmp/plt/$fileName")
         pw.write(result)
         pw.close
 
         // 結果をダウンロード
-        Redirect(routes.PLTConverterController.download(s"./tmp/converted/${fileName}"))
+        Redirect(routes.PLTConverterController.download(s"./tmp/plt/${fileName}"))
       }
       case None => Redirect(routes.PLTConverterController.index)
     }
